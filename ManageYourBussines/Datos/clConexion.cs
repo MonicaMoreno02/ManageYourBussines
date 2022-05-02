a679b007d57c6a70da958c59450aa55bf0defe04
Usuario <Usuario@LAPTOP-P1QT12CE>
2022-05-01 23:25:14 -0500

    Agregar archivos de proyecto.

---
+++ ManageYourBussines/Datos/clConexion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ManageYourBussines.Datos
+{
+    public class ClConexion
+    {
+        public class clConexion
+        {
+            SqlConnection conexion = null;
+            public clConexion()
+            {
+
+                conexion = new SqlConnection("");
+                conexion.Open();
+            }
+
+            public DataTable mtdDesconectado(string sql)//select
+            {
+                SqlDataAdapter adaptador = new SqlDataAdapter(sql, conexion);
+                DataTable tblDatos = new DataTable();
+                adaptador.Fill(tblDatos);
+                conexion.Close();
+                return tblDatos;
+            }
+
+            public int mtdConectado(string sql)//Insert,Update,Delete
+            {
+                SqlCommand comando = new SqlCommand(sql, conexion);
+                int filasAfectadas = comando.ExecuteNonQuery();
+                conexion.Close();
+                return filasAfectadas;
+            }
+        }
+    }
+}
---
+++ ManageYourBussines/Presentacion/frmProductos.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Presentacion/Site1.Master" AutoEventWireup="true" CodeBehind="frmProductos.aspx.cs" Inherits="ManageYourBussines.Presentacion.frmProductos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
