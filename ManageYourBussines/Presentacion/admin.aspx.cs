4917cafba59d46743a7c057467960a82d799555f
Ale Alba <maria.alba0602@gmail.com>
2022-05-05 23:42:29 -0500

    interfaz admin

---
+++ ManageYourBussines/Presentacion/admin.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManageYourBussines
+{
+    public partial class index : System.Web.UI.Page
+    {
+        
+    }
+}
