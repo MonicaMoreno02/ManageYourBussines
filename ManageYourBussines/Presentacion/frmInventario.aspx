617434139fdbccdcfb0e35a7e925a98b9b4d6030
oscar <ojramirez294@misena.edu.co>
2022-05-24 23:44:41 -0500

    sena

---
+++ ManageYourBussines/Presentacion/frmInventario.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Presentacion/Site1.Master" AutoEventWireup="true" CodeBehind="frmInventario.aspx.cs" Inherits="ManageYourBussines.Presentacion.frmInventario" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:GridView ID="gvInventario" runat="server">
+
+    </asp:GridView>
+
+</asp:Content>
---
+++ ManageYourBussines/Presentacion/frmInventario.aspx.cs
@@ -0,0 +1,21 @@
+﻿using ManageYourBussines.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManageYourBussines.Presentacion
+{
+    public partial class frmInventario : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            clProductoL objProductoL = new clProductoL();
+           gvInventario.DataSource = objProductoL.mtdListarP();
+            gvInventario.DataBind();
+
+        }
+    }
+}
