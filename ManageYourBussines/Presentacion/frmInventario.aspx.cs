a8b8b5e0399a2eb990c106af0601dfa18dfdf2a0
OSCARJULIANRAMIRES1210 <102269542+OSCARJULIANRAMIRES1210@users.noreply.github.com>
2022-05-26 10:36:24 -0500

    excel

--- ManageYourBussines/Presentacion/frmInventario.aspx.cs
+++ ManageYourBussines/Presentacion/frmInventario.aspx.cs
@@ -24,7 +24,6 @@ namespace ManageYourBussines.Presentacion
             DataTable inventario = new DataTable();
             inventario.Columns.Add(new DataColumn("#", typeof(int)));
             inventario.Columns.Add(new DataColumn("idproducto", typeof(string)));
-
             inventario.Columns.Add(new DataColumn("nombre", typeof(string)));
             inventario.Columns.Add(new DataColumn("describcion", typeof(string)));
             inventario.Columns.Add(new DataColumn("precio", typeof(float)));
@@ -109,7 +108,7 @@ namespace ManageYourBussines.Presentacion
 
             // Create test DataSet with five DataTables
             DataSet dataSet = new DataSet();
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 5; i++)
             {
                 clProductoL objProductoL = new clProductoL();
                 List<clProductoE> listInvent = new List<clProductoE>();
@@ -118,88 +117,63 @@ namespace ManageYourBussines.Presentacion
                 DataTable dataTable = new DataTable("Table " + (i + 1));
                 dataTable.Columns.Add(new DataColumn("#", typeof(int)));
                 dataTable.Columns.Add(new DataColumn("idproducto", typeof(string)));
-
                 dataTable.Columns.Add(new DataColumn("nombre", typeof(string)));
                 dataTable.Columns.Add(new DataColumn("describcion", typeof(string)));
                 dataTable.Columns.Add(new DataColumn("precio", typeof(float)));
                 dataTable.Columns.Add(new DataColumn("cantidadStock", typeof(int)));
                 dataTable.Columns.Add(new DataColumn("total", typeof(float)));
 
-
                 int cuenta = listInvent.Count;
                 int num = cuenta + 1;
                 float vtot = 0;
                 float total = 0;
-                for (int j = 0; i < num; i++)
+                for (int j = 0; j < cuenta; j++)
                 {
-
-
                     if (i != cuenta)
                     {
-                        int numero = i + 1;
-                        int idproducto = listInvent[i].idProducto;
-                        string nombre = listInvent[i].nombre;
-                        string describcion = listInvent[i].descripcion;
-                        float precio = listInvent[i].precio;
-                        int cantidadstok = listInvent[i].cantidadStock;
+                        int numero = j + 1;
+                        int idproducto = listInvent[j].idProducto;
+                        string nombre = listInvent[j].nombre;
+                        string describcion = listInvent[j].descripcion;
+                        float precio = listInvent[j].precio;
+                        int cantidadstok = listInvent[j].cantidadStock;
                         total = precio * cantidadstok;
                         vtot = vtot + total;
 
-                        DataRow row = dataTable.NewRow();
-                        row["#"] = numero;
-                        row["idproducto"] = idproducto;
-                        ;
-                        row["nombre"] = nombre;
-                        row["describcion"] = describcion;
-                        row["precio"] = precio;
-
-                        row["cantidadstock"] = cantidadstok;
-                        row["total"] = total;
-                        dataTable.Rows.Add(row);
-
-
-
-
-
-
+                        dataTable.Rows.Add(new object[] { numero, idproducto, nombre, describcion, precio, cantidadstok, total });
                     }
                     else
                     {
                         total = vtot;
 
-                        DataRow row = dataTable.NewRow();
-                        row["#"] = i + 1;
-
-                        row["nombre"] = "vtotal";
-                        row["describcion"] = "";
-                        row["precio"] = 0;
-
-                        row["cantidadstock"] = 0;
-                        row["total"] = total;
-                        dataTable.Rows.Add(row);
+                        dataTable.Rows.Add(new object[] { "", "", "", "", "", "", total });
 
                     }
-                    //dataSet.Tables.Add(dataTable);
                 }
 
-                // Create and fill a sheet for every DataTable in a DataSet
-                var workbook = new ExcelFile();
-                foreach (DataTable dataTable1 in dataSet.Tables)
-                {
-                    ExcelWorksheet worksheet = workbook.Worksheets.Add(dataTable.TableName);
 
-                    // Insert DataTable to an Excel worksheet.
-                    worksheet.InsertDataTable(dataTable,
-                        new InsertDataTableOptions()
-                        {
-                            ColumnHeaders = true
-                        });
-                }
 
-                workbook.Save("D:/file.xlsx");
+                dataSet.Tables.Add(dataTable);
+            }
+
+            // Create and fill a sheet for every DataTable in a DataSet
+            var workbook = new ExcelFile();
+            foreach (DataTable dataTable in dataSet.Tables)
+            {
+                ExcelWorksheet worksheet = workbook.Worksheets.Add(dataTable.TableName);
+
+                // Insert DataTable to an Excel worksheet.
+                worksheet.InsertDataTable(dataTable,
+                    new InsertDataTableOptions()
+                    {
+                        ColumnHeaders = true
+                    });
+            }
+
+            workbook.Save("B:/file.xlsx");
             }
         }
     }
-}
+
 
         
