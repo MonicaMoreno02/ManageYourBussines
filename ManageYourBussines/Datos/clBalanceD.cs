5be499f6bc1b09549ea5531da421a0a82f0cd9f2
OSCARJULIANRAMIRES1210 <102269542+OSCARJULIANRAMIRES1210@users.noreply.github.com>
2022-05-30 23:05:38 -0500

    generar reporte

---
+++ ManageYourBussines/Datos/clBalanceD.cs
@@ -0,0 +1,65 @@
+﻿using ManageYourBussines.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using static ManageYourBussines.Datos.ClConexion;
+
+namespace ManageYourBussines.Datos
+{
+    public class clBalanceD
+    {
+                public List<clBalanceE> mtdListar()
+        {
+            string sql = "select * from venta";
+            clConexion objConexion = new clConexion();
+            DataTable tblBalance = new DataTable();
+            tblBalance = objConexion.mtdDesconectado(sql);
+
+            List<clBalanceE> listarBal = new List<clBalanceE>();
+
+            int catnReg = tblBalance.Rows.Count;
+
+            for (int i = 0; i < catnReg; i++)
+            {
+                clBalanceE objbalance = new clBalanceE();
+                objbalance.idventa = int.Parse(tblBalance.Rows[i]["idVenta"].ToString());
+                objbalance.fechaVenta = DateTime.Parse(tblBalance.Rows[i]["fechaVenta"].ToString());
+                objbalance.codigoVenta=tblBalance.Rows[i]["codigoVenta"].ToString();
+                objbalance.totalVenta = int.Parse(tblBalance.Rows[i]["totalVenta"].ToString());
+                objbalance.idventa = int.Parse(tblBalance.Rows[i]["idVenta"].ToString()); ;
+                listarBal.Add(objbalance);
+            }
+
+            return listarBal;
+
+        }
+        
+             public List<clBalanceE> mtdListarDeta(clBalanceE objdetalles)
+        {
+            string sql = "select * from detallesVenta where idVenta="+objdetalles.idventa;
+            clConexion objConexion = new clConexion();
+            DataTable tblBalance = new DataTable();
+            tblBalance = objConexion.mtdDesconectado(sql);
+
+            List<clBalanceE> listarBal = new List<clBalanceE>();
+
+            int catnReg = tblBalance.Rows.Count;
+
+            for (int i = 0; i < catnReg; i++)
+            {
+                clBalanceE objbalance = new clBalanceE();
+                objbalance.idDetallesVenta = int.Parse(tblBalance.Rows[i]["idDetallesVenta"].ToString());
+                objbalance.idventa = int.Parse(tblBalance.Rows[i]["idventa"].ToString());
+                objbalance.idProducto = int.Parse(tblBalance.Rows[i]["idProducto"].ToString());
+                objbalance.cantidad = int.Parse(tblBalance.Rows[i]["cantidad"].ToString());
+                objbalance.valorTotal = float.Parse(tblBalance.Rows[i]["valorTotal"].ToString());
+                listarBal.Add(objbalance);
+            }
+
+            return listarBal;
+
+        }
+    }
+}
---
+++ ManageYourBussines/Presentacion/frmbalance.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Presentacion/Site1.Master" AutoEventWireup="true" CodeBehind="frmbalance.aspx.cs" Inherits="ManageYourBussines.Presentacion.frmbalance" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+      <div class="container">
+    <div class="col-md-6">
+    <asp:GridView ID="gvBalance" runat="server" AutoGenerateColumns="False"  class="table-secondary" Width="394px" OnRowCommand="gvBalance_RowCommand" >
+
+      <Columns>
+                           <asp:BoundField DataField="#" HeaderText="#">
+                           <ControlStyle Width="10px" />
+                           <ItemStyle Width="10px" />
+                           </asp:BoundField>
+                           <asp:BoundField DataField="idVenta" HeaderText="idVenta" />
+                           <asp:BoundField DataField="fechaVenta" HeaderText="fechaVenta" />
+                           <asp:BoundField DataField="codigoVenta" HeaderText="codigoVenta" />
+                           <asp:BoundField DataField="totalVenta" HeaderText="totalVenta" />
+                           <asp:ButtonField ButtonType="Button" CommandName="detalles" Text="detalles" />
+                       
+                       </Columns>
+    </asp:GridView>
+    <asp:GridView ID="gvDetalles" runat="server"></asp:GridView>
+
+        </div>
+          <div class="col-md-6">
+
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="excel" />
+          </div>
+          </div>
+</asp:Content>
