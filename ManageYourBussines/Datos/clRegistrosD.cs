95c377fc2146b4730050a50b3375331e0552f458
Ale Alba <maria.alba0602@gmail.com>
2022-06-08 22:27:29 -0500

    kkk

---
+++ ManageYourBussines/Datos/clRegistrosD.cs
@@ -0,0 +1,21 @@
+﻿using ManageYourBussines.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static ManageYourBussines.Datos.ClConexion;
+
+namespace ManageYourBussines.Datos
+{
+    //public int mtdRegistrarTareaD(clRegistroE objDatosRegistroD)
+    //{
+    //    string sql = "insert into tareas(nombreTarea, descripcionTarea) values('" + objDatosRegistroD.nombreTarea + "'," + objDatosRegistroD.descripcionTarea + "')";
+
+    //    clConexion objConexion = new clConexion();
+    //    int resultado = objConexion.mtdConectado(sql);
+    //    return resultado;
+
+    //}
+
+   
+}
