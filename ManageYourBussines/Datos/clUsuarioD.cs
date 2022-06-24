c574ad8d9ac22ba749dfd69ee9a23648aa99b7ce
OSCARJULIANRAMIRES1210 <102269542+OSCARJULIANRAMIRES1210@users.noreply.github.com>
2022-06-23 21:56:43 -0500

    cambios

--- ManageYourBussines/Datos/clUsuarioD.cs
+++ ManageYourBussines/Datos/clUsuarioD.cs
@@ -24,7 +24,7 @@ namespace ManageYourBussines.Datos
                 objDatosCliente.documento = tblDatos.Rows[0]["documento"].ToString();
                 objDatosCliente.nombreCliente = tblDatos.Rows[0]["nombreCliente"].ToString();
                 objDatosCliente.telefono = tblDatos.Rows[0]["telefono"].ToString();
-                objDatosCliente.apellidos = tblDatos.Rows[0]["apellidos"].ToString();
+                objDatosCliente.apellidos = tblDatos.Rows[0]["apellidosCliente"].ToString();
                 objDatosCliente.direccion = tblDatos.Rows[0]["direccion"].ToString();
                 objDatosCliente.emailCliente = tblDatos.Rows[0]["emailCliente"].ToString();
                 objDatosCliente.claveCliente = tblDatos.Rows[0]["claveCliente"].ToString();
