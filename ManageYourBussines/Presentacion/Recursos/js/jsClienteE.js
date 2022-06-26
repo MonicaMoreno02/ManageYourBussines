1dcba5f5136363245f2b312c6664e6af6c470c96
Dixon Fabian Salcedo Guecha <df.salcedo@mensajerosurbanos.com>
2022-06-25 22:48:32 -0500

    prueba de clientes

---
+++ ManageYourBussines/Presentacion/Recursos/js/jsClienteE.js
@@ -0,0 +1,55 @@
+﻿var tabla, datos
+sendDataAjax();
+function agregarFilas(datos) {
+    console.log(datos);
+    tabla = $("#dtClienteVendedor").dataTable();
+    for (var i = 0; i < datos.length; i++) {
+
+        tabla.fnAddData([
+            datos[i].idCliente,
+            datos[i].documento,
+            datos[i].nombreCliente,
+            datos[i].apellidos,
+            datos[i].telefono,
+            datos[i].direccion,
+            datos[i].emailCliente,
+            datos[i].claveCliente,
+            '<button type="button" id="btnDetallesV" class="btn btn-primary btn-details" data-toggle="modal" data-target="#ModalDetallesVendedor">Ver Detalles</button>'
+        ]);
+    }
+}
+
+function sendDataAjax() {
+    $.ajax({
+        type: "POST",
+        url: "frmClientesVendedor.aspx/mtdListarP",
+        data: '{}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+
+        success: function (datos) {
+            agregarFilas(datos.d);
+        },
+        failure: function (response) {
+            alert(response);
+        },
+        error: function (response) {
+            alert(response.d);
+        }
+
+    });
+
+    $(document).on('click', '#btnDetallesV', function (e) {
+
+        e.preventDefault();
+
+        var fila = $(this).parent().parent()[0];
+        var datosFila = tabla.fnGetData(fila);
+        var idCliente = datosFila[0];
+        $("#TextBox1").val(idCliente);
+
+
+    });
+
+
+}
---
+++ ManageYourBussines/Presentacion/frmClientesVendedor.aspx.cs
@@ -0,0 +1,55 @@
+﻿using ManageYourBussines.Entidades;
+using ManageYourBussines.Logica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManageYourBussines.Presentacion
+{
+    public partial class frmClientesVendedor : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        [WebMethod]
+        public static List<clUsuarioE> mtdListarP()
+        {
+            clClienteL objClienteL = new clClienteL();
+            List<clUsuarioE> objListaCliente = new List<clUsuarioE>();
+
+            objListaCliente = objClienteL.mtdListarL();
+
+            return objListaCliente;
+        }
+
+
+        //PAOLA:listar datos de ventas de cada cliente
+
+        protected void btnAceptar_Click1(object sender, EventArgs e)
+        {
+            dgModal.DataSource = null;
+
+            clClienteL objDatosVentas = new clClienteL();
+            List<clDetallesE> listaDetallesP = new List<clDetallesE>();
+
+            int idCliente = int.Parse(TextBox1.Text);
+            //int idCliente = 1;
+
+            listaDetallesP = objDatosVentas.mtdListarDetallesL(idCliente);
+
+            dgModal.DataSource = listaDetallesP;
+            dgModal.DataBind();
+        }
+
+        protected void btnCerrar_Click1(object sender, EventArgs e)
+        {
+            Response.Redirect("frmClientesVendedor.aspx");
+
+        }
+    }
+}
