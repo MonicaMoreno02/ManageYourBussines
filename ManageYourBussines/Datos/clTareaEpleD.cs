79920c0e041880903f93a28b9bf36dd615dccbc1
OSCARJULIANRAMIRES1210 <102269542+OSCARJULIANRAMIRES1210@users.noreply.github.com>
2022-06-30 00:22:05 -0500

    cambios

--- ManageYourBussines/Datos/clTareaEpleD.cs
+++ ManageYourBussines/Datos/clTareaEpleD.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using static ManageYourBussines.Datos.clConexion;
+using static ManageYourBussines.Datos.cguardar cambioslConexion;
 
 namespace ManageYourBussines.Datos
 {
